e16edf9ba5cf506225fc5edb9de19bfbe00a76aa
Geoffrey Grosenbach <boss@topfunky.com>
2009-10-23 15:04:54 -0700

    Implement task state checkbox widgets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,52 @@
+jQuery.fn.extend({
+
+  taskStates:['task-empty', 'task-x', 'task-apostrophe', 'task-dash'],
+
+  resetTaskStateClassNames:function() { 
+    var elements = this;
+    jQuery.each(jQuery.fn.taskStates, function() {
+      elements.removeClass(this);
+    })    
+    return this;
+  },
+
+  resetTaskState:function() {
+    this.resetTaskStateClassNames();
+    
+    return this.each(function() {
+      jQuery(this).data('taskStateIndex', 0)
+        .addClass(jQuery.fn.taskStates[0]);
+    });
+  },
+
+  toggleTaskState:function() { 
+    this.resetTaskStateClassNames();
+  
+    return this.each(function() {
+      var element = jQuery(this);
+      var taskStateIndex = element.data('taskStateIndex') || 0;
+      taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
+      
+      element.data('taskStateIndex', taskStateIndex)
+        .addClass(jQuery.fn.taskStates[taskStateIndex]);
+    });
+  },
+
+});
+
 jQuery(function () {
+
+  $('.completion a').live("click", function(e) {
+    $(this).toggleTaskState();
+    return false;
+  });
   
   $('#add').click(function(e) { 
     var taskItem = $('#tasks ul li:first').clone();
-    taskItem.find('form')[0].reset();
+    taskItem
+        .find('.completion a').resetTaskState()
+      .end()
+        .find('form')[0].reset();
     $('#tasks ul').append(taskItem);
     taskItem.find('input[type="text"]:first').focus();
     return false;
