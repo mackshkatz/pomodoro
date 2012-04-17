a7f6d84f4564e8ae261c9af3769fb0745ca7ee86
Max Katz <maxwelljkatz@gmail.com>
2012-04-17 10:36:07 -0600

    Fixed typo where I was checking equality instead of assigning like I wanted to. Finished pomodoro app

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,93 @@
+var CountdownTimer = {
+	endTime: null,
+	timerId: null,
+	minutes: 0,
+	maxWidth: 0,
+	maxTime: (25 * 60 * 1000),
+	isBreak: false,
+
+	normalColors: ["#e47a80", "#eb363b"],
+	breakColors: ["#97c751", "#759f58"],
+
+	start: function(minutes, isBreak) {
+		if (this.maxWidth == 0) {
+			this.maxWidth = $('#timer-bar').width();
+		}
+
+		if (this.timerId) {
+			this.cancel();
+		}
+
+		this.minutes = minutes;
+		this.isBreak = isBreak;
+
+		this.setupTimer();
+		this.flashBar();
+	},
+
+	setupTimer: function() {
+		$('#timer-finished').fadeOut();
+
+		var date = new Date();
+		this.endTime = date.getTime() + (this.minutes * 60 * 1000);
+		this.timerId = setInterval(this.tick, 1000);
+
+		this.setBarToTime(this.minutes * 60 * 1000);
+		$('title').text("Started " + (this.isBreak ? this.minutes + " minute break." : "pomodoro."));
+	},
+
+	flashBar: function() {
+		var lightColor, darkColor;
+		if (this.isBreak) {
+			lightColor = this.breakColors[0];
+			darkColor = this.breakColors[1];
+		} else {
+			lightColor = this.normalColors[0];
+			darkColor = this.normalColors[1];
+		}
+		$('#timer-bar').animate({backgroundColor:lightColor}).animate({backgroundColor:darkColor});
+	},
+
+	setBarToTime: function(time) {
+		$('#timer-bar').animate({width: (time/this.maxTime) * this.maxWidth});
+	},
+
+	cancel: function() {
+		clearInterval(this.timerId);
+	},
+
+	tick: function() {
+		// NOTE: Called externally, so much use "CountdownTimer" instead of "this".
+		CountdownTimer.tock();
+	},
+
+	tock: function() {
+		var date = new Date();
+		var remainingTime = this.endTime - date.getTime();
+		if (remainingTime <= 0) {
+			this.cancel();
+			this.setBarToTime(0);
+			this.showCompletedInLog();
+			this.showCompletedMessage();
+		} else {
+			this.setBarToTime(remainingTime);
+		}
+	},
+
+	showCompletedInLog: function() {
+		var logClass = "timer-log-" + this.minutes + (this.isBreak ? "-break" : "");
+		$('#timer-log').append('<div class="' + logClass + '"></div>');
+	},
+
+	showCompletedMessage: function() {
+		if ($('#timer-finished').length == 0)
+			$('#timer').append('<div id="timer-finished"></div>');
+		var message = "Finished " + (this.isBreak ? this.minutes + " minute break." : "one pomodoro.");
+		$('title').text(message);
+		$('#timer-finished').text(message).fadeIn(1000);
+	},
+};
+
 jQuery.fn.extend({
 	
 	taskStates: ['task-empty', 'task-x', 'task-apostrophe', 'task-dash'],
@@ -6,13 +96,12 @@ jQuery.fn.extend({
 		var elements = this;
 		jQuery.each(jQuery.fn.taskStates, function() {
 			elements.removeClass(this)
-		})
-
+		});
 		return this;
 	},
 
 	resetTaskState: function() {
-
+		this.resetTaskStateClassNames();
 		return this.each(function() {
 			jQuery(this).data('taskStateIndex', 0).addClass(jQuery.fn.taskStates[0]);
 		});
@@ -33,18 +122,30 @@ jQuery.fn.extend({
 
 jQuery(function () {
 
+	$('#button-25').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(25);
+	});
+
+	$('#button-5-break').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(5, true);
+	});
+
+	$('#button-25-break').click(function(e) {
+		e.preventDefault();
+		CountdownTimer.start(25, true);
+	});
+
 	$('.completion a').live("click", function(e) {
 		$(this).toggleTaskState();
 		return false;
 	})
 
-
 	$('#add').click(function(e) {
 		var taskItem =  $('#tasks ul li:first').clone();
-		taskItem
-			.find('.completion a').resetTaskState()
-			.end
-			.find('input[type="text"]').val("");
+		taskItem.find('.completion a').resetTaskState();
+		taskItem.find('input[type="text"]').val("");
 		$('#tasks ul').append(taskItem);
 		taskItem.find('input[type="text"]:first').focus();
 		return false;
